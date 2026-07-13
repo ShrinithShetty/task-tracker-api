34941e3363eefbdde53029e2dfcea1e022a5d450
ShrinithShetty <shettyshrinith03@gmail.com>
2026-07-13 22:06:37 +0530

    Completed FastAPI day 1 setup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.schemas.task import TaskCreate
 
 app = FastAPI()
 
@@ -6,3 +7,16 @@ app = FastAPI()
 
 def home():
     return {"message": "task_trackers is working"}
+
+@app.get('/health')
+
+def health():
+    return {"status" : "ok"}
+
+@app.post("/tasks")
+
+def task(task : TaskCreate):
+    return {
+        "message" : "task has been received successfully",
+        "task" : task
+    }
