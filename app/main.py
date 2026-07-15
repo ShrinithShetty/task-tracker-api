2a4a9ecd5894141fbbbf6ef68e2d6197b636030a
ShrinithShetty <shettyshrinith03@gmail.com>
2026-07-15 21:46:47 +0530

    Day 3: Update and Delete operation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.schemas.task import TaskCreate, TaskResponse
@@ -37,3 +37,34 @@ def create_task(task : TaskCreate, db: Session = Depends(get_db)):
 def get_task(db : Session = Depends(get_db)):
     tasks = db.query(Task).all()
     return tasks
+
+@app.put("/tasks/{task_id}",response_model = TaskResponse)
+def update_task(task_id : int, updated_task : TaskCreate, db: Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == task_id).first()
+
+    if task is None:
+        raise HTTPException(status_code = 404, detail = "Task Not Found" )
+    
+    task.title = updated_task.title
+    task.description = updated_task.description
+    task.completed = updated_task.completed
+
+    db.commit()
+    db.refresh(task)
+
+    return task
+
+@app.delete("/tasks/{task_id}", status_code = 204)
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(Task).filter(Task.id == task_id).first()
+
+    if task is None:
+        raise HTTPException(status_code = 404, detail = "Task Not Found")
+    
+    db.delete(task)
+    db.commit()
+
+    return None
+
+ 
+    
